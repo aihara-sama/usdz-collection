ece3fa0ae3f4935269ce4d5030886002caf0eaf8
Aihara <aihara-sama@mail.ru>
2023-11-24 13:59:23 +0200

    Initial commit from Create Next App

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+
+const Index: NextPage = () => {
+  // ~~~~~ Hooks ~~~~~
+
+  // ~~~~~ Cmp state ~~~~~
+
+  // ~~~~~ Refs ~~~~~
+
+  // ~~~~~ Vars ~~~~~
+
+  // ~~~~~ Effects ~~~~~
+
+  // ~~~~~ Handlers ~~~~~
+
+  // ~~~~~ JSX ~~~~~
+  return null;
+};
+
+export default Index;
